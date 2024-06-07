624139dca3a17189a3ef38daa1510ff72065cb1f
Erfan <erfansormi97@gmail.com>
2024-06-07 17:15:37 +0330

    created addresses page

--- src/app/profile/addresses/index.tsx
+++ src/app/profile/addresses/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
-import { View } from "react-native";
-// import Map from "@/components/common/map";
+import Card from "@/components/ui/card";
+import Text from "@/components/ui/text";
+import { AntDesign } from "@expo/vector-icons";
+import { Dimensions, View } from "react-native";
+import ProfileLayout from "../components/profile-layout";
 
 const Addresses = () => {
-  return <View className="flex-1">{/* <Map /> */}</View>;
+  return (
+    <ProfileLayout>
+      <Card className="bg-yellow-100/80">
+        <View className="flex-row" style={{ gap: 8 }}>
+          <AntDesign name="warning" size={24} color={"rgb(113 63 18)"} />
+          <View className="ml-1" style={{ width: Dimensions.get("window").width - 95 }}>
+            <Text className="text-yellow-800 pr-2 text-justify" style={{ lineHeight: 26 }}>
+              متاستفانه، بدلیل تحریم های موجود در استفاده sdk نقشه گوگل برای کشورمون ایران مانند
+              نیاز به credit card واقعی یا ناسازگاری با ip ایران و... موفق به استفاده از
+              react-native-maps نشدم!
+            </Text>
+          </View>
+        </View>
+      </Card>
+    </ProfileLayout>
+  );
 };
 
 export default Addresses;
